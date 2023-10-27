d0cf7e4986393ac8d916b58b649265f790189b0a
Hugo Pereira <hugocastroferro@gmail.com>
2023-10-27 09:59:22 +0100

    obstacle bug fix, speed, size, double the obstacles created

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ class Player {
   constructor() {
     // initialize properties
     this.height = 5;
-    this.width = 10;
+    this.width = 7;
     this.positionX = 50 - this.width / 2; // Center the player
     this.positionY = 0;
 
@@ -18,7 +18,7 @@ class Player {
   moveLeft() {
     if (this.positionX > 0) {
       // Prevent moving out of the screen
-      this.positionX--;
+      this.positionX-= 2;
       this.playerElm.style.left = this.positionX + "vw";
     }
   }
@@ -26,7 +26,7 @@ class Player {
   moveRight() {
     if (this.positionX + this.width < 100) {
       // Prevent moving out of the screen
-      this.positionX++;
+      this.positionX+= 2;
       this.playerElm.style.left = this.positionX + "vw";
     }
   }
@@ -38,37 +38,49 @@ class Obstacle {
     this.width = 10;
     this.positionX = Math.floor(Math.random() * (100 - this.width) + 1);
     this.positionY = 100;
-    this.obstacleElm = null;
+    this.obstacleElm1 = null;
+    this.obstacleElm2 = null;
 
     this.createDomElement();
   }
   createDomElement() {
     // step1: create the element
-    this.obstacleElm = document.createElement("div");
+    this.obstacleElm1 = document.createElement("div");
+    this.obstacleElm2 = document.createElement("div");
 
     // step2: add content or modify
-    this.obstacleElm.classList.add("obstacle");
-    this.obstacleElm.style.width = this.width + "vw";
-    this.obstacleElm.style.height = this.height + "vh";
-    this.obstacleElm.style.left = this.positionX + "vw";
-    this.obstacleElm.style.bottom = this.positionY + "vh";
+     this.obstacleElm1.classList.add("obstacle");
+    this.obstacleElm1.style.width = this.width + "vw";
+    this.obstacleElm1.style.height = this.height + "vh";
+    this.obstacleElm1.style.left = this.positionX + "vw";
+    this.obstacleElm1.style.bottom = this.positionY + "vh";
 
-    // step3: append to the dom: `parentElm.appendChild()`
-    const parentElm = document.getElementById("board");
-    parentElm.appendChild(this.obstacleElm);
-  }
+    // step2: add content or modify for the second obstacle
+    this.obstacleElm2.classList.add("obstacle");
+    this.obstacleElm2.style.width = this.width + "vw";
+    this.obstacleElm2.style.height = this.height + "vh";
+    this.obstacleElm2.style.left = this.positionX + "vw";
+    this.obstacleElm2.style.bottom = this.positionY + "vh";
 
-  // modifying position and updating UI
+     // step3: append to DOM both obstcles to parent
+     const parentElm = document.getElementById("board");
+     parentElm.appendChild(this.obstacleElm1);
+     parentElm.appendChild(this.obstacleElm2);
+   }
+
+  // modifying position and updating UI for both obstacles
   moveDown() {
     this.positionY--;
-    this.obstacleElm.style.bottom = this.positionY + "vh";
+    this.obstacleElm1.style.bottom = this.positionY + "vh";
+    this.obstacleElm2.style.bottom = this.positionY + "vh";
 
-    // Remove the obstacle from DOM
-    if (this.positionY < 0 - this.obstacleElm.height) {
-      this.obstacleElm.remove();
+    // Remove both obstacles from the DOM when either one goes out of view
+    if (this.positionY < 0 - this.height) {
+      this.obstacleElm1.remove();
+      this.obstacleElm2.remove();
 
-    // remove from array
-    obstacleArr.shift()
+      // Remove both obstacles from the array
+      obstacleArr.shift();
     }
   }
 }
@@ -81,7 +93,7 @@ const obstacleArr = []; // will store instances of the class Obstacle
 setInterval(() => {
   const newObstacle = new Obstacle();
   obstacleArr.push(newObstacle);
-}, 350);
+}, 300);
 
 //update obstacles
 setInterval(() => {
@@ -100,7 +112,7 @@ setInterval(() => {
       //location.href = "./gameover.html"
     }
   });
-}, 30);
+}, 20);
 
 document.addEventListener("keydown", (e) => {
   switch (e.code) {
