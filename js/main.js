c12bf3d306f3e5f1ff9f18650cafba303bee16dc
Hugo Pereira <hugocastroferro@gmail.com>
2023-10-27 08:27:12 +0100

    fix moving bugs and add limits to player

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 class Player {
   constructor() {
     // initialize properties
-    this.positionX = 50;
-    this.positionY = 0;
     this.height = 20;
     this.width = 30;
+    this.positionX = 50 - this.width / 2; // Center the player
+    this.positionY = 0;
 
     // dom manipulation to reflect initial values (size, position)
     this.playerElm = document.getElementById("player");
@@ -14,24 +14,28 @@ class Player {
     this.playerElm.style.left = this.positionX + "vw";
     this.playerElm.style.bottom = this.positionY + "vh";
   }
+
   moveLeft() {
-    this.positionX--;
-
-    this.playerElm.style.left = this.positionX + "vw";
+    if (this.positionX > 0) { // Prevent moving out of the screen
+      this.positionX--;
+      this.playerElm.style.left = this.positionX + "vw";
+    }
   }
-  moveRight() {
-    this.positionX++;
 
-    this.playerElm.style.left = this.positionX + "vw";
+  moveRight() {
+    if (this.positionX + this.width < 100) { // Prevent moving out of the screen
+      this.positionX++;
+      this.playerElm.style.left = this.positionX + "vw";
+    }
   }
 }
 
 class Obstacle {
   constructor() {
-    this.positionX = 50;
-    this.positionY = 100;
     this.height = 10;
     this.width = 30;
+    this.positionX = Math.random() * (100 - this.width);
+    this.positionY = 100;
     this.obstacleElm = null;
 
     this.createDomElement();
@@ -99,11 +103,11 @@ document.addEventListener("keydown", (e) => {
   switch (e.code) {
     case "ArrowLeft":
       console.log("pressed left");
-      player.moveLeft;
+      player.moveLeft(); // Call the moveLeft function
       break;
     case "ArrowRight":
       console.log("pressed right");
-      player.moveRight;
+      player.moveRight(); // Call the moveRight function
       break;
   }
 });
